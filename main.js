e7733f0f558396d6cdcbccabff0959ab1dad6c9f
HarukaNishida <nishidaharuka.41@gmail.com>
2021-07-25 18:28:37 +0900

    作業内容

---
+++ main.js
@@ -0,0 +1,241 @@
+var img;
+
+function sampleimg1() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/a-gazou.jpeg";
+}
+
+function sampleimg2() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/i-gazou.jpeg";
+}
+
+function sampleimg3() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/u-gazou.jpg";
+}
+
+function sampleimg4() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/e-gazou.jpg";
+}
+
+function sampleimg5() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/o-gazou.jpg";
+}
+
+function sampleimg6() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ka-gazou.jpg";
+}
+
+function sampleimg7() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ki-gazou.jpg";
+}
+
+function sampleimg8() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ku-gazou.jpg";
+}
+
+function sampleimg9() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ke-gazou.jpg";
+}
+
+function sampleimg10() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ko-gazou.jpg";
+}
+
+function sampleimg11() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/sa-gazou.jpg";
+}
+
+function sampleimg12() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/shi-gazou.jpg";
+}
+
+function sampleimg13() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/su-gazou.jpg";
+}
+
+function sampleimg14() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/se-gazou.jpg";
+}
+
+function sampleimg15() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/so-gazou.jpg";
+}
+
+function sampleimg16() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ta-gazou.jpg";
+}
+
+function sampleimg17() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/chi-gazou.jpg";
+}
+
+function sampleimg18() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/tsu-gazou.jpg";
+}
+
+function sampleimg19() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/te-gazou.jpg";
+}
+
+function sampleimg20() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/to-gazou.jpg";
+}
+
+function sampleimg21() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/na-gazou.jpg";
+}
+
+function sampleimg22() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ni-gazou.jpg";
+}
+
+function sampleimg23() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/nu-gazou.jpg";
+}
+
+function sampleimg24() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ne-gazou.jpg";
+}
+
+function sampleimg25() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/no-gazou.jpg";
+}
+
+function sampleimg26() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ha-gazou.jpg";
+}
+
+function sampleimg27() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/hi-gazou.jpg";
+}
+
+function sampleimg28() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/hu-gazou.jpg";
+}
+
+function sampleimg29() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/he-gazou.jpg";
+}
+
+function sampleimg30() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ho-gazou.jpg";
+}
+
+function sampleimg31() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ma-gazou.jpg";
+}
+
+function sampleimg32() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/mi-gazou.jpg";
+}
+
+function sampleimg33() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/mu-gazou.jpg";
+}
+
+function sampleimg34() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/me-gazou.jpg";
+}
+
+function sampleimg35() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/mo-gazou.jpg";
+}
+
+function sampleimg36() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ya-gazou.jpg";
+}
+
+function sampleimg38() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/yu-gazou.jpg";
+}
+
+function sampleimg40() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/yo-gazou.jpg";
+}
+
+function sampleimg41() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ra-gazou.jpg";
+}
+
+function sampleimg42() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ri-gazou.jpg";
+}
+
+function sampleimg43() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ru-gazou.jpg";
+}
+
+function sampleimg44() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/re-gazou.jpg";
+}
+
+function sampleimg45() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/ro-gazou.jpg";
+}
+
+function sampleimg46() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/wa-gazou.jpg";
+}
+
+function sampleimg47() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/wi-gazou.jpg";
+}
+
+function sampleimg49() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/we-gazou.jpg";
+}
+
+function sampleimg50() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/wo-gazou.jpg";
+}
+
+function sampleimg51() {
+  img = document.getElementById("image_file");
+  img.src = "img/henntaigana/nn-gazou.jpg";
+}
